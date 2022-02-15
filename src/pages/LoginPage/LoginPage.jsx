7b0f826cf3487033fd2b5fe48a0e5adb0503db06
takumi1 <25145731+takumi1@users.noreply.github.com>
2022-02-16 02:26:33 +0700

    Login and registration validation

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,28 +1,83 @@
-import React from 'react';
-import { Link, useHistory  } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 // import WhiteButton from '../../UI/Buttons/whiteButton';
 import BlueButton from '../../UI/Buttons/blueButton';
 import { ReactComponent as Logo } from '../../UI/Images/Logo.svg';
 import './LoginPage.css';
-import getUsers from '../../services/api/getUsers';
+// import getUsers from '../../services/api/getUsers';
 import AuthService from '../../services/api/auth.service';
+import {useHistory} from "react-router";
 
 const LoginPage = () => {
   const history = useHistory();
   const login = React.createRef();
-  const password = React.createRef();
+  const pass = React.createRef();
+  const [loginError, setLoginError] = useState(false);
   const handleSubmit = () => {
-    // if (login.current.value.length > 128) return
-    // if (password.current.value.length < 8) return
-    AuthService.login(login.current.value, password.current.value).then(
+    // if (login.current.value.length > 128) return console.log('too long name');
+    // if (password.current.value.length < 8) return console.log('8 chars minimum');
+    AuthService.login(email, password).then(
       () => {history.push("/boards");},
     ).catch((error) => {
       console.log({ error });
+      setLoginError(true)
+
     });
   };
-  const getUs = () => {
-    console.log(getUsers());
-  };
+  // const getUs = () => {
+  //   console.log(getUsers());
+  // };
+  /* eslint-disable */
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [emailError, setEmailError] = useState('Email не может быть пустым');
+  const [passwordError, setPasswordError] = useState('Пароль не может быть пустым');
+  const [formValid, setFormValid] = useState(false)
+
+  useEffect(() => {
+if (emailError || passwordError) {
+  setFormValid(false)
+} else {
+  setFormValid(true)
+}
+  }, [emailError, passwordError])
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value)
+    const re = /\S+@\S+\.\S+/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError('Некорректный E-mail')
+    } else {
+      setEmailError('')
+    }
+  }
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value)
+    if(e.target.value.length < 8) {
+      setPasswordError('Пароль должен быть не менее 8 символов')
+      if(!e.target.value) {
+        setPasswordError('Пароль не может быть пустым')
+      }
+    } else {
+      setPasswordError()
+    }
+  }
+
+  /* eslint-enable */
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'email':
+        setEmailDirty(true)
+            break;
+      case 'password':
+        setPasswordDirty(true)
+        break;
+
+    }
+  }
   return (
     <div
       className="auth"
@@ -32,22 +87,28 @@ const LoginPage = () => {
       >
         <Link to="/"><Logo className="logotype" /></Link>
         <div className="inputWrapper">
-          <div className="inputName">E-mail:</div>
-          <input type="text" required ref={login} />
-          <div className="inputName">Password:</div>
-          <input type="text" ref={password} />
+          <div className="inputName">E-mail*:</div>
+          <input onChange={e => emailHandler(e)} defaultValue={email}
+                 onBlur={e => blurHandler(e)} name='email' type="text" required ref={login} />
+          {(emailDirty && emailError) && <div style={{color: 'red'}}>{emailError}</div>}
+          <div className="inputName">Password*:</div>
+          <input onChange={e => passwordHandler(e)} defaultValue={password}
+                 onBlur={e => blurHandler(e)} name='password' type="text" ref={pass} />
+          {(passwordDirty && passwordError) && <div style={{color: 'red'}}>{passwordError}</div>}
 
           <div className="signIn">
-            <button type="button" aria-label="Sign In" className="whiteButton" onClick={handleSubmit}>
+            <button disabled={!formValid} type="button"
+                    aria-label="Sign In" className="whiteButton" onClick={handleSubmit}>
               Sign In
             </button>
-
-            <button type="button" aria-label="Sign In" className="whiteButton" onClick={getUs}>
-              Get Users
-            </button>
+            {loginError ? <div style={{color: 'red'}}>Такой пользователь не найден</div> : ''}
+            {/*<button type="button" aria-label="Sign In" className="whiteButton" onClick={getUs}>*/}
+            {/*  Get Users*/}
+            {/*</button>*/}
           </div>
           <hr />
-          <div className="signUp"><Link to="/reg"><BlueButton title="Sign Up" /></Link></div>
+          <div className="signUp"><Link to="/reg"><BlueButton title="Sign Up" /></Link>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,20 +1,105 @@
-import React from 'react';
-import { Link, useHistory  } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 // import WhiteButton from '../../UI/Buttons/whiteButton';
 import BlueButton from '../../UI/Buttons/blueButton';
 import { ReactComponent as Logo } from '../../UI/Images/Logo.svg';
 import './RegistrationPage.css';
 import AuthService from '../../services/api/auth.service';
+import {useHistory} from "react-router";
 
 const LoginPage = () => {
-  const history = useHistory ();
+  const history = useHistory();
   const login = React.createRef();
-  const password = React.createRef();
+  const pass = React.createRef();
   const name = React.createRef();
+
+  const nameCheck = (e, email) => {
+    if (e) {
+     return e
+    } else {
+      return email.split('@')[0]
+    }
+  }
   const handleSubmit = () => {
-    AuthService.register(login.current.value, name.current.value, password.current.value)
+    AuthService.register(email, nameCheck(name.current.value, email), password)
         .then((res)=> {history.push("/boards")});
   };
+
+  /* eslint-disable */
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [emailError, setEmailError] = useState('Email не может быть пустым');
+  const [passwordError, setPasswordError] = useState('Пароль не может быть пустым');
+  const [formValid, setFormValid] = useState(false)
+
+  useEffect(() => {
+    if (emailError || passwordError) {
+      setFormValid(false)
+    } else {
+      setFormValid(true)
+    }
+  }, [emailError, passwordError])
+
+
+
+  const latinValid = (e, err) => {
+    const reg = /^[A-z\0-9\u00C0-\u00ff\s'\.,-\/#@!$%\^&\*;:{}=\-_`~()]+$/;
+    if (e.target.value && !reg.test(String(e.target.value).toLowerCase())) {
+      err('Только латиница!')
+      console.log('only latin')
+      return false
+    } else {
+      setEmailError('')
+      console.log('ok')
+      return true
+    }
+  }
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const reg = /\S+@\S+\.\S+/;
+    if (latinValid(e, setEmailError)) {
+      if (e.target.value && !reg.test(String(e.target.value).toLowerCase())) {
+        setEmailError('Некорректный E-mail')
+      } else if(!e.target.value) {
+          setEmailError('E-mail не может быть пустым')
+          setFormValid(true)
+        } else {
+        setEmailError('')
+        return true
+      }
+    }
+
+  }
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value)
+    if (latinValid(e, setEmailError)) {
+      if (e.target.value.length < 8) {
+        setPasswordError('Пароль должен быть не менее 8 символов')
+        if (!e.target.value) {
+          setPasswordError('Пароль не может быть пустым')
+        }
+      } else {
+        setPasswordError()
+      }
+    }
+  }
+
+  /* eslint-enable */
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'email':
+        setEmailDirty(true)
+        break;
+      case 'password':
+        setPasswordDirty(true)
+        break;
+
+    }
+  }
   return (
     <div
       className="auth"
@@ -24,15 +109,20 @@ const LoginPage = () => {
       >
         <Link to="/"><Logo className="logotype" /></Link>
         <div className="inputWrapper">
-          <div className="inputName">E-mail:</div>
-          <input type="text" required ref={login} />
-          <div className="inputName">Password:</div>
-          <input type="text" required ref={password} />
+          <div className="inputName">E-mail*:</div>
+          <input onChange={e => emailHandler(e)} defaultValue={email}
+                 onBlur={e => blurHandler(e)} name='email' type="text" required ref={login} />
+          {(emailDirty && emailError) && <div style={{color: 'red'}}>{emailError}</div>}
+          <div className="inputName">Password*:</div>
+          <input onChange={e => passwordHandler(e)} defaultValue={password}
+                 onBlur={e => blurHandler(e)} name='password' type="text" ref={pass} />
+          {(passwordDirty && passwordError) && <div style={{color: 'red'}}>{passwordError}</div>}
           <div className="inputName">Name:</div>
           <input type="text" ref={name} />
 
           <div className="signUp">
-            <button type="button" aria-label="Sign In" className="whiteButton" onClick={handleSubmit}>
+            <button disabled={!formValid} type="button"
+                    aria-label="Sign In" className="whiteButton" onClick={handleSubmit}>
               Sign Up
             </button>
           </div>
