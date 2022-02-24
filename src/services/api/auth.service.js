ed4eac692d81041d142b8b3249e0d2ae9dcb6d7f
takumi1 <25145731+takumi1@users.noreply.github.com>
2022-02-24 21:51:20 +0700

    Корректная авторизация пользователя сразу после регистрации.

--- src/services/api/auth.service.js
+++ src/services/api/auth.service.js
@@ -7,6 +7,7 @@ const register = (email, name, password) => axios.post(`${API_URL}auth/signup`, 
   password,
 }).then((response) => {
   console.log(response);
+  login(email, password);
   return response.data;
 });
 const login = (email, password) => axios
