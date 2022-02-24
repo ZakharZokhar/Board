c66177ffa2a9fd6964bb80926abf7900eb69721e
takumi1 <25145731+takumi1@users.noreply.github.com>
2022-02-24 21:37:13 +0700

    Убрана перезагрузка страницы.

--- src/services/api/deleteUser.js
+++ src/services/api/deleteUser.js
@@ -5,6 +5,5 @@ export const deleteUsers = (id) => {
 const apiUrl = `http://173.212.214.70:3004/users/delete/${id}`;
 axios.delete(apiUrl, authHeader()).then((resp) => {
     alert('Пользователь удалён!')
-    window.location.reload();
 })
 };
